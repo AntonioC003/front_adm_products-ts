ba40c3c2835e25fde08aa6604dff1e4b9e1ec331
AntonioC003 <ma.castilloquintanilla@ugto.mx>
2026-03-10 14:56:32 -0600

    Creando el routing para editar producto por id con un loader

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom'
 import type { Product } from "../types"
 import { formatCurrency } from "../utils"
 
@@ -8,6 +9,8 @@ type ProductDetailsProps = {
 
 export default function ProductDetails({product} : ProductDetailsProps) {
 
+  // useNavigate se puede usar en cualquier parte del codigo
+  const navigate = useNavigate()
   const isAvailability = product.availability
   return (
     <tr className="border-b ">
@@ -21,7 +24,16 @@ export default function ProductDetails({product} : ProductDetailsProps) {
           {isAvailability ? 'Disponible': 'No disponible'}
         </td>
         <td className="p-3 text-lg text-gray-800 ">
-          
+          <div className="flex gap-2 items-center">
+            {/*<Link
+              to={`/productos/${product.id}/editar`}
+              className='bg-indigo-600 text-white text-center rounded-lg w-full p-2 uppercase font-bold text-ts'
+            >Editar</Link> */}
+            <button
+              onClick={() => navigate(`/productos/${product.id}/editar`)}
+              className='bg-indigo-600 text-white text-center rounded-lg w-full p-2 uppercase font-bold text-ts'
+            >Editar</button>
+          </div>
         </td>
     </tr> 
   )
---
+++ src/views/EditProduct.tsx
@@ -0,0 +1,82 @@
+import { Form, Link, useActionData, type ActionFunctionArgs, redirect, type LoaderFunctionArgs } from "react-router-dom";
+import ErrorMessage from "../components/ErrorMessage";
+import { addProduct } from "../services/ProductService";
+
+
+
+export async function loader({params} : LoaderFunctionArgs) {
+
+}
+
+export async function action({request}: ActionFunctionArgs) {
+  // Recuperando los datos del usuario sin useState
+  const data = Object.fromEntries( await request.formData())
+
+  let error = ''
+  // -Validando campos
+  if (Object.values(data).includes('')){
+    error = 'Todos los campos son obligatorios'
+  }
+  if(error.length){
+    return error
+  }
+
+  await addProduct(data)
+  return redirect('/')
+}
+
+
+export default function EditProduct() {
+
+  // Captura de nuevo las acciones con useActionData()
+  const error = useActionData() as string
+  
+  return (
+    <>
+      <div className="flex justify-between mx-5">
+        <h2 className="text-4xl font-black text-slate-500">Editar Producto</h2>
+        <Link
+          to="/"
+          className="rounded-md bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500 "
+          >Volver a Productos</Link>
+      </div>
+      {error && <ErrorMessage>{error}</ErrorMessage>  }
+      <Form
+        method="POST"
+        className="mt-10"      
+      >
+        <div className="mb-4">
+          <label
+              className="text-gray-800"
+              htmlFor="name"
+            >Nombre Producto:</label>
+            <input 
+              id="name"
+              type="text"
+              className="mt-2 block w-full p-3 bg-gray-50"
+              placeholder="Nombre del Producto"
+              name="name"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="text-gray-800"
+              htmlFor="price"
+            >Precio:</label>
+            <input 
+              id="price"
+              type="number"
+              className="mt-2 block w-full p-3 bg-gray-50"
+              placeholder="Precio Producto. ej. 200, 300"
+              name="price"
+            />
+          </div>
+          <input
+            type="submit"
+            className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
+            value="Registrar Producto"
+          />
+        </Form>
+    </>
+  )
+}
